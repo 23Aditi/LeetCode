0f0f4cd9eb191becff689eaf549de57fb6fd96f2
23Aditi <aditi23github@gmail.com>
2024-11-09 18:10:20 +0530

    Time: 0 ms (100.00%), Space: 9 MB (56.65%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+       /*
+        //Approach 1 :
         unordered_map<int,vector<int>> um;
         int count = 0;
         for(auto& row : matrix){
@@ -14,5 +16,14 @@ public:
                 matrix[j][size-i] = row[j];
             } 
         }
+        */
+        //Approach 2 :
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = i + 1  ; j < matrix.size() ; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i = 0 ; i < matrix.size() ; i++){
+            reverse(matrix[i].begin() , matrix[i].end());  }
     }
 };
