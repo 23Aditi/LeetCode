ed4277a472a75c459f8ec7977e2c00bdf7a91a1d
ADITI <aditi23github@gmail.com>
2025-02-06 16:12:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int,int> um;
+        int n = nums.size();
+        // product and freq
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                um[nums[i]*nums[j]]++;
+            }
+        }
+        int ct = 0;
+        for(auto p : um){
+            if(p.second == 2){ct++;}
+            if(p.second>2){
+                ct+=p.second;
+            }
+        }
+    //count 3 ; 3 pairs a , b ; a , c ; c, b
+        return ct*8;
+    }
+};
