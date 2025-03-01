e35ecdb833c799b98390557c72aeb116825fc9c5
ADITI <aditi23github@gmail.com>
2025-03-01 12:03:55 +0530

    Time: 2 ms (12.65%), Space: 13.3 MB (23.58%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i= 0 ; i < n-1 ; i++){
+            if(nums[i]==nums[i+1]){
+                nums[i] = nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i]==0){
+                int j = i+1;
+                while(j<n && nums[j]==0){
+                    j++;
+                }
+                if(j<n) swap(nums[i],nums[j]);
+            }
+        }
+        return nums;
+    }
+};
