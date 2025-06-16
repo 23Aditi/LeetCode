abfe2beb270469d59cf059ad03fb05d009ebd83e
ADITI <aditi23github@gmail.com>
2025-06-16 19:14:04 +0530

    Time: 0 ms (100%), Space: 29.6 MB (57.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int length = nums.size();
-        k%=length;
-        if(k==0){return;}
-        
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-        
+        int n = nums.size();
+        int r= k%n;
+        if(n==0 || r==0){return;}
+        std::rotate(nums.begin(),nums.end()-r,nums.end());
     }
-    /*void rotateArray(vector<int>& nums){
-        int last = nums[nums.size()-1];
-        for(int i = nums.size()-1 ; i>0 ; i-- ){
-            nums[i]=nums[i-1];
-        }
-        nums[0] = last;
-    }*/
 };
