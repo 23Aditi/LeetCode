f5748996f1b3531e0ab945575aaaee3167aea1c4
ADITI <aditi23github@gmail.com>
2025-10-05 10:17:42 +0530

    Time: 1 ms (13.65%), Space: 17.4 MB (89.6%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,23 +1,13 @@
 class Solution {
-    int climbCost(vector<int>& cost, int n,vector<int>& dp) {
-        
-        if (n < 0) return 0;
-        if (n == 0 || n == 1) return cost[n];
-        if(dp[n-1]==-1){
-            dp[n-1]=climbCost(cost,n-1,dp);
-        }
-        if(dp[n-2]==-1){
-            dp[n-2]=climbCost(cost,n-2,dp);
-        }
-        return cost[n] + min(dp[n-1],dp[n-2]);
-    }
-
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n,-1);
-        // The final position is after the last step, so we're calculating
-        // the minimum cost to reach position n from either n-1 or n-2
-        return min(climbCost(cost, n-1,dp), climbCost(cost, n-2,dp));
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[n];
     }
 };
