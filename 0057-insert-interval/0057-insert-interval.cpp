f5e31355ac2fb136334189f6a0f13c885ecbead3
ADITI <aditi23github@gmail.com>
2025-10-11 13:41:18 +0530

    Time: 3 ms (31.71%), Space: 21.8 MB (20.87%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans = {{intervals[0][0],intervals[0][1]}};
+        for(int i = 1 ; i < intervals.size(); i++){
+            int n = ans.size();
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if(start<=ans[n-1][1] && end>ans[n-1][1]){
+                ans[n-1][1]=end;
+            }else if(start>ans[n-1][1]){
+                ans.push_back({start,end});
+            }
+        }
+        return ans;
+    }
+};
