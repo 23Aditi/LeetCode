43e85eda74899490595b5f12bdc20351fd2b3733
ADITI <aditi23github@gmail.com>
2025-11-19 13:10:05 +0530

    Time: 2 ms (73.54%), Space: 14.5 MB (61.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int k) {
-        unordered_map<int,int> um;
+        // unordered_map<int,int> um;
+        // int n = nums.size();
+        // for(int i = 0 ; i < n ; i++){
+        //     um[nums[i]]=i;
+        // }
+        // for(int i = 0 ; i < n ; i++){
+        //     if(um.find(k-nums[i])!=um.end() && um[k-nums[i]]!=i){
+        //         return {i,um[k-nums[i]]};
+        //     }
+        // }
+        // return {-1,-1};
+
+        ////// approach 2
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            um[nums[i]]=i;
+        vector<int> idx(n,0);
+        for(int i = 0 ; i < (int)nums.size(); i++){
+            idx[i]=i;
         }
-        for(int i = 0 ; i < n ; i++){
-            if(um.find(k-nums[i])!=um.end() && um[k-nums[i]]!=i){
-                return {i,um[k-nums[i]]};
-            }
+        sort(idx.begin(),idx.end(),[&nums](auto &a , auto &b){
+            return nums[a]<nums[b];
+        });
+        int i = 0;
+        int j = nums.size()-1;
+        while(i<j){
+            if(nums[idx[i]]+nums[idx[j]]==k){return {idx[i],idx[j]};}
+            else if(nums[idx[i]]+nums[idx[j]]>k){j--;}
+            else{i++;}
         }
         return {-1,-1};
     }
