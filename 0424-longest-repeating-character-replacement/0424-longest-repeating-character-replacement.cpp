a89daef44afbe34dc5b0510923045e85279641e5
ADITI <aditi23github@gmail.com>
2026-01-07 21:45:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        vector<int> vec(26,0);
         int n = s.size();
-        int j = 0;
+        if(k==0){
+            int maxi = 1;
+            int len = 1;
+            for(int i = 0 ; i < n-1 ; i++){
+                if(s[i]==s[i+1])len++;
+                else len=1;
+                maxi = max(maxi,len);
+            }
+            return maxi;
+        }
         int maxFreq = 0;
-        char c;
+        int j = 0;
         int maxlen = 0;
-        unordered_map<char,int> um;
-        for(int i = 0 ; i < n ; i++){
-            um[s[i]]++;
-            if(maxFreq<um[s[i]]){
-                maxFreq = um[s[i]];
+        char c = '.';
+        for(int i = 0 ; i < n; i++){
+            vec[s[i]-'A']++;
+            if(vec[s[i]-'A']>maxFreq){
+                maxFreq = vec[s[i]-'A'];
                 c = s[i];
             }
-            while(i-j+1-maxFreq>k){
-                um[s[j]]--;
-                int maxi = 0;
-                for(auto [k,v]:um){
-                    maxi = max(maxi,v);
-                }
-                maxFreq=maxi;
+            while(i-j+1 > maxFreq+k){
+                vec[s[j]-'A']--;
+                if(s[j]==c){maxFreq--;}
                 j++;
             }
             maxlen = max(maxlen,i-j+1);
