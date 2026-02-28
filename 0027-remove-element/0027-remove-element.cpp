74c58497ab71b7f68e51f6cd7f9b04f1c89bb9fc
ADITI <aditi23github@gmail.com>
2026-02-28 15:45:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count = 0;
-        for(int i = 0 ; i < nums.size(); i++){
-            if(nums[i]!=val){
-                count++;
-            }else{
-               nums.erase(nums.begin()+i); i--;
-            }
+        int n = nums.size();
+        if(n==0) return n;
+        if(n==1) return n!=val;
+        int i = 0 , j = n - 1;
+        while(i<j){
+            if(nums[i]!=val){i++;continue;}
+            if(nums[j]==val){j--;continue;}
+            swap(nums[i],nums[j]);
         }
-        return count;
+        return j;
     }
 };
