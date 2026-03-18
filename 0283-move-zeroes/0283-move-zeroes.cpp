299bd23b2f60ade6bbfdccf39be40bd93371e167
ADITI <aditi23github@gmail.com>
2026-03-18 17:49:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        for(int j = 1 ; j < nums.size(); j++){
-            if(nums[i]!=0 && i < nums.size()-1) i++;
-            if(nums[i]==0 && nums[j]!=0){
-                swap(nums[i],nums[j]);
-            }
+        int n = nums.size();
+        int i = 0; // track zeros
+        for(int j = 1 ; j < n ; j++){
+            if(nums[i]==0 && nums[j]!=0){swap(nums[i],nums[j]);}
+            if(nums[i]!=0 && i<n)i++;
         }
     }
 };
