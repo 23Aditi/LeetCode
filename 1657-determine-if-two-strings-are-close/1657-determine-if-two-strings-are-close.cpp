e96618077f1cf2be92151cf4fff9e01f94fadfb4
ADITI <aditi23github@gmail.com>
2026-03-20 21:34:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        unordered_map<char,int> um1;
-        unordered_map<char,int> um2;
-        if(word1.size()!=word2.size()) return false;
-        for(int i = 0 ; i < word1.size(); i++){um1[word1[i]]++;}
-        for(int i = 0 ; i < word2.size(); i++){um2[word2[i]]++;}
-        vector<int> freq1, freq2;
-        for(auto i : um1){
-            if(um2.find(i.first)==um2.end()){
-                // element absent 
+        vector<int> freq1(26,0), freq2(26,0);
+        for(auto x : word1){freq1[x-'a']++;}
+        for(auto x : word2){freq2[x-'a']++;}
+        for(int i = 0 ; i < 26 ; i++){
+            if((freq1[i]==0 && freq2[i])||(freq2[i]==0 && freq1[i])){
                 return false;
-            }else{
-                freq1.push_back(i.second);
-            }
-        }
-        for(auto i : um2){
-            if(um1.find(i.first)==um1.end()){
-                return false;
-            }else{
-                freq2.push_back(i.second);
             }
         }
         sort(freq1.begin(),freq1.end());
         sort(freq2.begin(),freq2.end());
-        return freq1==freq2;
+        return freq1 == freq2;
     }
 };
