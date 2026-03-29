82fdfeadbf1962371aa373aecc1fca2964e8b3c6
ADITI <aditi23github@gmail.com>
2026-03-29 17:57:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -7,12 +7,27 @@ class Solution {
             }
         }
     }
+    void bfs(vector<vector<int>>& rooms, vector<int>& visited, int node){
+        queue<int> q;
+        q.push(0);
+        visited[0]=1;
+        while(!q.empty()){
+            int t = q.front();
+            q.pop();
+            for(auto x : rooms[t]){
+                if(!visited[x]){
+                    visited[x]=1;
+                    q.push(x);
+                }
+            }
+        }
+    }
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<int> visited(n,0);
         visited[0]=1;
-        dfs(rooms,visited,0);
+        bfs(rooms,visited,0);
         int sum = accumulate(visited.begin(),visited.end(),0);
         cout << sum << endl;
         return sum==n?true:false;
