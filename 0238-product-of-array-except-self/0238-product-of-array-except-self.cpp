2bbdf8c11f3fa65d3cc5d07382c223a14aeb5a8c
ADITI <aditi23github@gmail.com>
2026-04-05 19:33:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,20 +1,34 @@
 class Solution {
+
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        long long product = 1;
+        // long long product = 1;
+        // int n = nums.size();
+        // vector<int> result(n,0);
+        // int zero = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i]){product*=nums[i];}
+        //     else{zero++;}
+        // }
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i] && !zero){
+        //         result[i]=product/nums[i];
+        //     }else if(!nums[i] && zero==1){
+        //         result[i]=product;
+        //     }
+        // }
+        // return result;
         int n = nums.size();
-        vector<int> result(n,0);
-        int zero = 0;
+        vector<int> result(n,1);
+        int prefix = 1;
         for(int i = 0 ; i < n ; i++){
-            if(nums[i]){product*=nums[i];}
-            else{zero++;}
+            result[i]*= prefix;
+            prefix *= nums[i];
         }
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] && !zero){
-                result[i]=product/nums[i];
-            }else if(!nums[i] && zero==1){
-                result[i]=product;
-            }
+        int subfix = 1;
+        for(int i = n-1 ; i >=0 ; i--){
+            result[i]*= subfix;
+            subfix *= nums[i];
         }
         return result;
     }
