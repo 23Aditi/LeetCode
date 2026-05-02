461eef109c04541b5ae4312267086767d12f528c
ADITI <aditi23github@gmail.com>
2026-05-02 16:20:38 +0530

    Time: 2 ms (40.88%), Space: 9.7 MB (18.77%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,23 +1,21 @@
 class Solution {
-    vector<int> vec = {0,1,5,-1,-1,2,9,-1,8,6};
-    bool func(int n){
-        int num = n;
-        bool changing = 0;
-        while(num){
-            int r = num%10;
-            if(vec[r]==-1)return false;
-            if(vec[r]!=r) changing = true;
-            num/=10;
-        }
-        return changing;
-    }
+    vector<int> vec={0,0,1,-1,-1,1,1,-1,0,1};
 public:
     int rotatedDigits(int n) {
+        vector<int> dp(n+1,0);
         int ct = 0;
-        for(int i = 1 ; i <=n ; i++){
-            if(func(i)){
+        for(int i = 1 ; i <= n ; i++){
+            int prefix = i/10;
+            int last = i%10;
+            // dp[i]=0 -> unchangable , dp[i]=-1 -> invalid and dp[i]=1 for changable
+            if(dp[prefix]==-1 || vec[last]==-1){
+                dp[i]=-1; // 33 , 31 , 41 , 42
+            }
+            else if(dp[prefix]==1 || vec[last]==1){
+                dp[i]=1;
                 ct++;
-                cout << i << endl;
+            }else{
+                dp[i]=0;
             }
         }
         return ct;
